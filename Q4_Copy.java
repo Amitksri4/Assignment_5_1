0852f76f6aa90a35fd14e16bc8d9be2332016867
Amitksri4 <amit.ksri4@gmail.com>
2017-02-09 19:26:00 +0530

    Add files via upload

---
+++ Q4_Copy.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Copy{
+  public static void main(String[] argv) throws Exception {
+    Set set1 = new HashSet();
+    Set set2 = new HashSet();
+
+    set1.addAll(set1);
+
+  }
+}
